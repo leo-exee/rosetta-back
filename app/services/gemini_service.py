257df4e4aff5c19c75f57d9bdd8cab37be7136ec
leo-exee <leopold@littlebill.io>
2025-06-05 19:46:11 -0400

    fix(gemini): fix 503 error by remplacing the model

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -1,14 +1,33 @@
+import time
+
 from google.genai import types
+from google.genai.errors import ServerError
 
 from app.config.gemini_config import GeminiClient
 
 
-def generate_gemini_content_service(prompt: str) -> str | None:
-    response = GeminiClient.models.generate_content(
-        model="gemini-2.0-flash",
-        config=types.GenerateContentConfig(
-            system_instruction="Tu es un assistant pédagogique expert en FLE (français langue étrangère)."
-        ),
-        contents=prompt,
-    )
-    return response.text
+def generate_gemini_content_service(
+    prompt: str, max_retries: int = 3, delay: float = 1.0
+) -> str | None:
+
+    for attempt in range(max_retries):
+        try:
+            response = GeminiClient.models.generate_content(
+                model="gemini-2.5-flash-preview-05-20",
+                config=types.GenerateContentConfig(
+                    system_instruction="Tu es un assistant pédagogique expert en FLE (français langue étrangère)."
+                ),
+                contents=prompt,
+            )
+            return response.text
+
+        except ServerError as e:
+            if hasattr(e, "code") and e.code == 503 and attempt < max_retries - 1:
+                time.sleep(delay)
+                delay *= 1.5
+                continue
+            else:
+                raise e
+        except Exception as e:
+            raise e
+    return None
